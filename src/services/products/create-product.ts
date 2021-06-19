1a8bc4b22d913a9a6b609b7fc2629e7b466cdcf5
graysoncunha <grayson@tamborine.com.br>
2021-06-19 07:33:19 -0300

    fix(product): inclui tipagem no retorno das funções

--- src/services/products/create-product.ts
+++ src/services/products/create-product.ts
@@ -2,8 +2,7 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
 import { ProductModel } from '../../models/product.model';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default async function createProduct(fastify: FastifyInstance) {
+export default async function createProduct(fastify: FastifyInstance): Promise<void> {
   fastify.post('/products', {}, async (req: FastifyRequest, reply: FastifyReply) => {
     const dadosProduto = req.body;
 
--- src/services/products/list-products.ts
+++ src/services/products/list-products.ts
@@ -2,8 +2,7 @@ import { FastifyInstance, FastifyReply } from 'fastify';
 
 import { ProductModel } from '../../models/product.model';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default async function listProducts(fastify: FastifyInstance) {
+export default async function listProducts(fastify: FastifyInstance): Promise<void> {
   fastify.get('/products', {}, async (_, reply: FastifyReply) => {
     const products = await ProductModel.find();
 
