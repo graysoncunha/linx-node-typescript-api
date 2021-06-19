f445af534162b3442d9307313c1978eef37f9959
graysoncunha <grayson@tamborine.com.br>
2021-06-19 07:44:57 -0300

    feat(credit-card): inclui model para acessar a collection credicard

---
+++ src/models/credit-card.model.ts
@@ -0,0 +1,53 @@
+import { Document, Schema, model, Model } from 'mongoose';
+
+export interface ICreditCard {
+  cardNumber: string;
+  cardHolderName: string;
+  totalToPay: number;
+  value: number;
+  cvv: number;
+  exp_date: string;
+}
+
+export interface ICreditCardDocument extends Document {
+  cardNumber: string;
+  cardHolderName: string;
+  totalToPay: number;
+  value: number;
+  cvv: number;
+  exp_date: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type TypeCreditCardModel = Model<ICreditCardDocument>;
+
+export const crediCardSchema: Schema<ICreditCard> = new Schema<ICreditCard>(
+  {
+    cardNumber: {
+      type: String,
+      required: true,
+    },
+    cardHolderName: {
+      type: Number,
+      required: true,
+    },
+    totalToPay: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    exp_date: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const CreditCardModel = model<ICreditCardDocument, TypeCreditCardModel>('CreditCard', crediCardSchema);
