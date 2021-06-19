9bf1c8b60ba3e8a429ce92cfbc93422e9d92fd12
graysoncunha <grayson@tamborine.com.br>
2021-06-19 08:03:08 -0300

    feat(transaction): inclui model para entidade transaction

---
+++ src/models/transaction.model.ts
@@ -0,0 +1,54 @@
+import { Document, Schema, model, Model } from 'mongoose';
+
+export interface ITransaction {
+  client: string;
+  clientName: string;
+  totalToPay: number;
+  creditCard: string;
+}
+
+export interface ITransactionDocument extends Document {
+  client: string;
+  clientName: string;
+  totalToPay: number;
+  creditCard: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type TypeTransactionModel = Model<ITransactionDocument>;
+
+export const transactionSchema: Schema<ITransaction> = new Schema<ITransaction>(
+  {
+    client: {
+      type: String,
+      required: true,
+    },
+    clientName: {
+      type: String,
+      required: true,
+    },
+    totalToPay: {
+      type: String,
+      required: true,
+    },
+    creditCard: {
+      type: String,
+      required: true,
+    },
+    exp_date: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const TransactionModel = model<ITransactionDocument, TypeTransactionModel>(
+  'Transaction',
+  transactionSchema,
+  'transactions'
+);
