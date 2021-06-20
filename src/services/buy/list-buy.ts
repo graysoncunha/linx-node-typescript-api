194ce004ea12379c7c1f6f791722b841edcb4c69
graysoncunha <grayson@tamborine.com.br>
2021-06-20 12:32:00 -0300

    fix(buy): corrige o código da resposta http

--- src/services/buy/list-buy.ts
+++ src/services/buy/list-buy.ts
@@ -6,6 +6,6 @@ export default async function createProduct(fastify: FastifyInstance): Promise<v
   fastify.get('/starstore/history', {}, async (_, reply: FastifyReply) => {
     const trasactions = await TransactionModel.find();
 
-    reply.status(201).send(trasactions);
+    reply.status(200).send(trasactions);
   });
 }
