6448c34f6e558288ef7ff1852569887e20fddbde
graysoncunha <grayson@tamborine.com.br>
2021-06-20 20:15:02 -0300

    fix: inclui tipo na função

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import buildApp from './app';
 
-export default async function startServer(): Promise<void> {
+async function startServer(): Promise<void> {
   const server = buildApp();
 
   const port = 3000;
 
   await server.listen(port);
 }
+
+startServer();
